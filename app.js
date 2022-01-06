af0a52dbb8db015019e90ed3a02762c9437556cc
usa3amustafa <usa3a3ustafa@outlook.com>
2022-01-06 20:45:47 +0500

    homepage

--- app.js
+++ app.js
@@ -14,3 +14,30 @@ collection.addEventListener('click', () => {
 
   collectionDropdown.classList.toggle('show')
 })
+
+const counterText = document.querySelector('.counter')
+const decrease = document.querySelector('.btn-decrease')
+const increase = document.querySelector('.btn-increase')
+
+let counterNumber = 0
+
+const resetFunction = function () {
+  counterNumber = 0
+  //   counterText.className = ''
+  counterText.textContent = counterNumber
+}
+
+const decreaseFunction = function () {
+  counterNumber > 0 ? counterNumber-- : 0
+  counterText.textContent = counterNumber
+}
+
+const increaseFunction = function () {
+  counterNumber++
+  counterText.textContent = counterNumber
+}
+
+resetFunction()
+
+decrease.addEventListener('click', decreaseFunction)
+increase.addEventListener('click', increaseFunction)
